c2a07d54c5988dcc6c1dd12ecc6092ce4574f867
TashaLee9002 <153100612+TashaLee9002@users.noreply.github.com>
2024-01-09 12:46:40 -0700

    Add files via upload

---
+++ ShoppingCartItem.cs
@@ -0,0 +1,32 @@
+﻿
+namespace CKK.Logic.Models
+{
+   public class ShoppingCartItem
+   {
+      private Product _Product;
+      private int Quantity;
+
+      public ShoppingCartItem(Product product, int quantity)
+      {
+         _Product = product;
+         Quantity = quantity;
+      }
+
+      public int GetQuantity()
+      {
+         return Quantity;
+      }
+      public void SetQuantity(int quantity)
+      {
+         Quantity = quantity;
+      }
+      public Product GetProduct()
+      {
+         return _Product;
+      }
+      public void SetProduct(Product product)
+      {
+         _Product = product;
+      }
+   }
+}
---
+++ StoreItem.cs
@@ -0,0 +1,32 @@
+﻿
+namespace CKK.Logic.Models
+{
+   public class StoreItem
+   {
+      private Product _product;
+      private int _quantity;
+
+      public StoreItem(Product product, int quantity)
+      {
+         _product = product;
+         _quantity = quantity;
+      }
+
+      public int GetStoreQuantity()
+      {
+         return _quantity;
+      }
+      public void SetStoreQuantity(int quantity)
+      {
+         _quantity = quantity;
+      }
+      public Product GetStoreProduct()
+      {
+         return _product;
+      }
+      public void SetStoreProduct(Product product)
+      {
+         _product = product;
+      }
+   }
+}
